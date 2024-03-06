37ef747b7cc4cc99fb61e89af660bd59eafee5f5
GHKEN <tetsuya@ghken.com>
2024-03-06 11:26:43 +0900

    chartjsを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,30 @@
+import { useEffect } from 'react';
+import {
+    Chart as ChartJS,
+    ArcElement,
+    Tooltip,
+    Legend,
+    type ChartData,
+} from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 export default function Home() {
-  return (
-    <main>hoge</main>
-  );
+    const dataset: ChartData<'doughnut'> = {
+        labels: ['a', 'b', 'c'],
+        datasets: [
+            {
+                label: 'dataset2',
+                data: [1, 2, 3],
+                backgroundColor: ['red', 'green', 'blue'],
+            },
+        ],
+    };
+
+    return (
+        <main>
+            <Doughnut data={dataset} />
+        </main>
+    );
 }
